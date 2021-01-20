9344031d442015f4b1fb3d85dd338a0794e82438
digamo <digamo@gmail.com>
2021-01-19 22:03:43 -0300

    Primeiro commit do projeto.

---
+++ src/main/java/br/com/digamo/pontoeletronico/api/PontoEletronicoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.digamo.pontoeletronico.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoEletronicoApiApplication.class, args);
+	}
+
+}
